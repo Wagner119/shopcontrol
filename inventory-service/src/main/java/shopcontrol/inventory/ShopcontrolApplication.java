fdd532c7b6b09b82fcf012bef40ec3a41a27a0ec
“Wagner“ <matthias.wagner@adesso.de>
2026-03-29 10:59:07 +0200

    Initial commit: inventory-service scaffold

---
+++ inventory-service/src/main/java/shopcontrol/inventory/ShopcontrolApplication.java
@@ -0,0 +1,13 @@
+package shopcontrol.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopcontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopcontrolApplication.class, args);
+    }
+
+}
